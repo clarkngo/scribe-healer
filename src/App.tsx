cc176efea9264c0778166b9f4c34604046f83f5e
Clark Ngo <clarkngo@gmail.com>
2022-12-29 01:12:43 -0800

    add initial antd button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Button } from 'antd';
 import logo from './logo.svg';
+import 'antd/dist/reset.css';
 import './App.css';
 
 function App() {
@@ -8,8 +10,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First edit
+          First button with Antd 
         </p>
+        <Button type="primary">Button</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
