f3e30caf0fb1737aeb56485894fa6fd3071934b3
Clark Ngo <clark@clarks-air.lan>
2022-12-29 01:02:01 -0800

    first edit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          First edit
         </p>
         <a
           className="App-link"
