efcb08e2f39e78775b16d38d78b468e273ccc8d7
Clark Ngo <clarkngo@gmail.com>
2022-12-29 04:07:16 -0800

    initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,84 @@
 import React from 'react';
-import { Button } from 'antd';
-import logo from './logo.svg';
-import 'antd/dist/reset.css';
-import './App.css';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  BorderTopOutlined,
+  BulbTwoTone,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Layout, Menu, theme,  } from 'antd';
+import { Typography } from 'antd';
 
-function App() {
+const { Title } = Typography;
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps['items'] = [
+  {
+    key: "volleyball",
+    icon: React.createElement(UserOutlined),
+    label: "Volleyball"
+  },
+  {
+    key: "movies",
+    icon: React.createElement(UserOutlined),
+    label: "Movies"
+  },
+  {
+    key: "tech",
+    icon: React.createElement(UserOutlined),
+    label: "Tech"
+  },
+];
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          First button with Antd 
-        </p>
-        <Button type="primary">Button</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Header style={{ padding: 0, background: colorBgContainer }} >
+          <Title level={2}>Scribe Healer</Title>
+        </Header>
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
+            <p>long content</p>
+              {
+                // indicates very long content
+                Array.from({ length: 100 }, (_, index) => (
+                  <React.Fragment key={index}>
+                    {index % 20 === 0 && index ? 'more' : '...'}
+                    <br />
+                  </React.Fragment>
+                ))
+              }
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
