b0a6385f12329214917f4cb71b38fa9a1b11c634
Clark Ngo <clarkngo@gmail.com>
2022-12-30 02:15:14 -0800

    read json better

--- src/pokemons/MockPokemons.ts
+++ src/pokemons/MockPokemons.ts
@@ -1,11 +1,2 @@
-import { Pokemon } from './Pokemon'
-import { Pokemons } from './Pokemons';
-import pokemonObj from './pokemons.json'
-
-// export const MOCK_POKEMONS = [
-    
-// ]
-// let pokemon = Object.assign(new Pokemon, pokemonObj);
-let pokemon = <Pokemon> JSON.parse(JSON.stringify(pokemonObj));
-console.log(pokemon)
+let pokemon = require('./pokemons.json');
 export default pokemon;
