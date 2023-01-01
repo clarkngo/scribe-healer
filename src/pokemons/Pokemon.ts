d85074f7fb5e7a88887f966a075a20a82321c708
Clark Ngo <clarkngo@gmail.com>
2022-12-31 23:45:06 -0800

    add pokemon types tag

--- src/pokemons/Pokemon.ts
+++ src/pokemons/Pokemon.ts
@@ -1,3 +1,29 @@
-export class Pokemon {
-    id: string | undefined;;
+import React from "react";
+
+export interface Pokemon {
+    id?: string;
+    name?: Name ;
+    type?: string[]; 
+    base?: Base;
+}
+
+export interface Name {
+    english: string;
+    japanese: string;
+    chinese: string;
+    french: string;
+}
+
+export interface Base {
+    HP: string;
+    Attack: string;
+    Defense: string;
+    "Sp. Attack": string;
+    "Sp. Defense": string;
+    Speed?: string;
+}
+
+export let TYPES = {
+    "Grass": 'green',
+    "Poison": 'purple',
 }
